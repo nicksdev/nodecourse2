1a351e39f467ffa3a8ff0068b933b13451e2367e
Nick Hughes <nick@nickhughes.io>
2022-09-09 08:54:01 +1000

    Assignment 1 Final Commit

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -3,16 +3,31 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Welcome to the First Page</title></head>');
-        res.write('<body><form action="/users" method="POST"><input type="text" name="user"><button type="submit>Send</button></form></body>');
+        
+       //res.write('<body><form><input type="text"><button></button></form></body>');
+        
+        res.write('<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
     }
     
-    if (url === '/users' && method === 'POST') {
-        fs.writeFile('')
+    if (url === '/create-user' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);            
+        });
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=');
+            console.log(message[1]);
+        });
+        return res.end();
+
     }
     
     if (url === '/users') {
