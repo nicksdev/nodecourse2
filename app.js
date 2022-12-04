c4ad9b7ccc87f58fda2e3ad7614924f63395f740
Nick Hughes <nick@nickhughes.io>
2022-12-05 09:04:20 +1100

    Routes Start

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-const http = require('http');
-
 const express = require('express');
-
 const app = express();
 
+
 app.use((req, res, next) => {
-   console.log("In the middleware!"); 
-   next();
+   console.log('In another middleware!!')
+   res.send('<h1>Hello from Express</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3001);
 
 
