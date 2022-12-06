71347367ff3fa6e3a07b816dcf04066a73b11def
Nick Hughes <nick@nickhughes.io>
2022-12-07 09:51:49 +1100

    Routes 2

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 
 
-app.use((req, res, next) => {
-   console.log('In another middleware!!')
-   res.send('<h1>Hello from Express</h1>');
+app.use('/add-product', (req, res, next) => {
+   console.log('Log Add Product Route')
+   res.send('<h1>Add Product Route</h1>');
 });
+
+
+
+app.use('/', (req, res, next) => {
+   console.log('Log Default Route')
+   res.send('<h1>Default Route</h1>');
+});
+
 
 app.listen(3001);
 
